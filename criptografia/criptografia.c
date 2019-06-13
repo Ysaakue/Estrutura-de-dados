3090d99106dd8ae9509c4e13d4f21e643a00063e
Ysaakue <isaacgondim82@gmail.com>
2019-06-13 11:51:55 -0300

    Funções de criptografar e descriptografar

--- criptografia/criptografia.c
+++ criptografia/criptografia.c
@@ -9,6 +9,8 @@ void continuar();
 char *recebeString(char *string);
 void CriaArquivo(FILE *arquivo);
 void LerDoArquivo(FILE *arquivo);
+void CriptografarArquivo(FILE *arquivo);
+void DescriptografarArquivo(FILE *arquivo);
 
 // FUNCAO PRINCIPAL ***********************************************************
 
@@ -24,6 +26,8 @@ int main()
         printf("________________________________________________\n");
         printf("| 1) CRIAR UMA MENSAGEM                         |\n");
         printf("| 2) LER MENSAGEM                               |\n");
+        printf("| 3) CRIPITOGRAFAR MENSAGEM                     |\n");
+        printf("| 4) DESCRIPTOGRAFAR MENSAGEM                   |\n");
         printf("|                                               |\n");
         printf("| 0) SAIR                                       |\n");
         printf("|_______________________________________________|\n");
@@ -44,10 +48,11 @@ int main()
             break;
         
         case 3:
+            CriptografarArquivo(Save);
             break;
 
         case 4:
-            
+            DescriptografarArquivo(Save);
             break;
 
         default:
@@ -109,13 +114,9 @@ void CriaArquivo(FILE *arquivo)
     int opcao;
 
     printf("Digite o nome do arquivo:");
-    // strcpy(NomeArquivo,"teste.bin");
-//    scanf("%s", NomeArquivo);
 	fflush(stdin);
 	gets(NomeArquivo);
     printf("\nDigite a Mensagem para ser colocada no arquivo:");
-    // scanf("%s", mensagem);
-//    mensagem = recebeString(mensagem);
 	gets(mensagem);
     int tamanho = strlen(mensagem);
     do{
@@ -184,8 +185,6 @@ void LerDoArquivo(FILE *arquivo)
     int opcao;
 	
     printf("Digite o nome do arquivo:");
-    // strcpy(NomeArquivo,"teste.bin");
-//    scanf("%s", NomeArquivo);
 	fflush(stdin);
 	gets(NomeArquivo);
 
@@ -268,4 +267,346 @@ void LerDoArquivo(FILE *arquivo)
     }while(opcao <0 || opcao>2);
 }
 
+//-----------------------------------------------------------------------------
+
+void CriptografarArquivo(FILE *arquivo) 
+{
+	char NomeArquivo[30], mensagem[30], operacao[2];
+    int opcao, primeiro_numero, primeira_operacao,i,numero_usado;
+	
+    printf("Digite o nome do arquivo:");
+	fflush(stdin);
+	gets(NomeArquivo);
+
+    do{
+        system("cls");
+
+        printf("       *** SELECIONAR TIPO DE ARQUIVO ***       \n");
+        printf("________________________________________________\n");
+        printf("| 1) TEXTO                                      |\n");
+        printf("| 2) BINARIA                                    |\n");
+        printf("|                                               |\n");
+        printf("| 0) SAIR                                       |\n");
+        printf("|_______________________________________________|\n");
+        printf("OPCAO:");
+        fflush(stdin);
+        scanf("%d",&opcao);
+
+        switch(opcao) 
+        {
+            case 0: break;
+
+            case 1:
+                if((arquivo=fopen(NomeArquivo, "r"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA LEITURA COM SUCESSO.\n",NomeArquivo);
+                    if(fread(&mensagem, 30*sizeof(char), 1, arquivo) != 1 )
+                    {
+                        printf("ERRO NA LEITURA");
+                    }
+                    fclose(arquivo);
+                }
+                
+                primeiro_numero = rand() % 100;
+                primeira_operacao = rand() % 2;
+                i=0;
+
+                if(primeira_operacao)
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2==0)
+                        {
+                            mensagem[i] = mensagem[i]+(numero_usado++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]-(numero_usado++);
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2!=0)
+                        {
+                            mensagem[i] = mensagem[i]+(numero_usado++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]-(numero_usado++);
+                        }
+                    }
+                    i++;
+                }
+
+                printf("\n\nmensagem:%s \nprimeiro numero: %d\nprimeira operacao: %d\n\n", mensagem, primeiro_numero, primeira_operacao);
+                
+
+                if((arquivo=fopen(NomeArquivo, "r+"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA GRAVACAO COM SUCESSO.\n",NomeArquivo);
+                    if(!fwrite(&mensagem, 30*sizeof(char), 1, arquivo) || !fwrite(&primeiro_numero, sizeof(int), 1, arquivo) || !fwrite(&primeira_operacao, sizeof(int), 1, arquivo)) printf("ERRO NA ESCRITA\n");
+                    else puts("GRAVACAO REALIZADA COM SUCESSO\n");
+                    fclose(arquivo);
+                }
+                continuar();
+                break;
+            
+            case 2:
+                if((arquivo=fopen(NomeArquivo, "rb"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA LEITURA COM SUCESSO.\n",NomeArquivo);
+                    if(fread(&mensagem, 30*sizeof(char), 1, arquivo) != 1 )
+                    {
+                        printf("ERRO NA LEITURA");
+                    }
+                    fclose(arquivo);
+                }
+
+                primeiro_numero = rand() % 100;
+                primeira_operacao = rand() % 2;
+                i=0;
+                
+
+                if(primeira_operacao)
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2==0)
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2!=0)
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                    }
+                    i++;
+                }
+
+                if((arquivo=fopen(NomeArquivo, "w+b"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA GRAVACAO COM SUCESSO.\n",NomeArquivo);
+                    if(!fwrite(&mensagem, 30*sizeof(char), 1, arquivo) || !fwrite(&primeiro_numero, sizeof(int), 1, arquivo) || !fwrite(&primeira_operacao, sizeof(int), 1, arquivo)) printf("ERRO NA ESCRITA\n");
+                    else puts("GRAVACAO REALIZADA COM SUCESSO\n");
+                    printf("%s",mensagem);
+                    fclose(arquivo);
+                }
+                continuar();
+                break;
+ 
+            default:
+                printf("   *** OPCAO INVALIDA   ***");
+                break;
+        }
+    }while(opcao <0 || opcao>2);
+}
+
+//-----------------------------------------------------------------------------
+
+void DescriptografarArquivo(FILE *arquivo) 
+{
+	char NomeArquivo[30], mensagem[30], operacao[2];
+    int opcao,primeiro_numero,primeira_operacao,i;
+	
+    printf("Digite o nome do arquivo:");
+	fflush(stdin);
+	gets(NomeArquivo);
+
+    do{
+        system("cls");
+
+        printf("       *** SELECIONAR TIPO DE ARQUIVO ***       \n");
+        printf("________________________________________________\n");
+        printf("| 1) TEXTO                                      |\n");
+        printf("| 2) BINARIA                                    |\n");
+        printf("|                                               |\n");
+        printf("| 0) SAIR                                       |\n");
+        printf("|_______________________________________________|\n");
+        printf("OPCAO:");
+        fflush(stdin);
+        scanf("%d",&opcao);
+
+        switch(opcao) 
+        {
+            case 0: break;
+
+            case 1:
+                if((arquivo=fopen(NomeArquivo, "r"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA LEITURA COM SUCESSO.\n",NomeArquivo);
+                    if(fread(&mensagem, 30*sizeof(char), 1, arquivo) != 1 || fread(&primeiro_numero, sizeof(int), 1, arquivo) != 1 || fread(&primeira_operacao, sizeof(int), 1, arquivo) != 1 )
+                    {
+                        printf("ERRO NA LEITURA");
+                    }
+                    fclose(arquivo);
+                }
+
+                printf("\n\nmensagem:%s \nprimeiro numero: %d\nprimeira operacao: %d\n\n", mensagem, primeiro_numero, primeira_operacao);
+
+
+                i=0;
+                if(primeira_operacao)
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2==0)
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2!=0)
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                    }
+                    i++;
+                }
+                
+
+                if((arquivo=fopen(NomeArquivo, "r+"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA GRAVACAO COM SUCESSO.\n",NomeArquivo);
+                    if(!fwrite(&mensagem, 30*sizeof(char), 1, arquivo) ) printf("ERRO NA ESCRITA\n");
+                    else puts("GRAVACAO REALIZADA COM SUCESSO\n");
+                    fclose(arquivo);
+                }
+                continuar();
+                break;
+            
+            case 2:
+                if((arquivo=fopen(NomeArquivo, "rb"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA LEITURA COM SUCESSO.\n",NomeArquivo);
+                    if(fread(&mensagem, 30*sizeof(char), 1, arquivo) != 1 || fread(&primeiro_numero, sizeof(int), 1, arquivo) != 1 || fread(&primeira_operacao, sizeof(int), 1, arquivo) != 1  )
+                    {
+                        printf("ERRO NA LEITURA");
+                    }
+                    fclose(arquivo);
+                }
+
+                i=0;
+                if(primeira_operacao)
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2==0)
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    while(mensagem[i]!='\0')
+                    {
+                        if(i%2!=0)
+                        {
+                            mensagem[i] = mensagem[i]-(primeiro_numero++);
+                        }
+                        else
+                        {
+                            mensagem[i] = mensagem[i]+(primeiro_numero++);
+                        }
+                    }
+                    i++;
+                }
+
+                if((arquivo=fopen(NomeArquivo, "w+b"))==NULL)
+                {
+                    printf("\nERRO AO ABRIR O ARQUIVO '%s'.\n",NomeArquivo);
+                    perror("fopen");
+                }
+                else
+                {
+                    printf("\nARQUIVO '%s' ABERTO PARA GRAVACAO COM SUCESSO.\n",NomeArquivo);
+                    if(!fwrite(&mensagem, 30*sizeof(char), 1, arquivo) ) printf("ERRO NA ESCRITA\n");
+                    else puts("GRAVACAO REALIZADA COM SUCESSO\n");
+                    printf("%s",mensagem);
+                    fclose(arquivo);
+                }
+                continuar();
+                break;
+ 
+            default:
+                printf("   *** OPCAO INVALIDA   ***");
+                break;
+        }
+    }while(opcao <0 || opcao>2);
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
